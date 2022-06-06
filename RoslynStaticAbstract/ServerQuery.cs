16b48cbe494ad9d50e0abc73dc25c613d4012130
Chase Redmon <redmon.chase@gmail.com>
2022-06-06 19:52:14 -0400

    Init

---
+++ RoslynStaticAbstract/ServerQuery.cs
@@ -0,0 +1,14 @@
+﻿namespace RoslynStaticAbstract;
+
+public interface IStatic<out T>
+{
+    static abstract T Parse(ReadOnlySpan<byte> text);
+}
+
+public class ServerQuery : IStatic<ServerQuery>
+{
+    public static ServerQuery Parse(ReadOnlySpan<byte> text)
+    {
+        throw new NotImplementedException();
+    }
+}
